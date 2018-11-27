a35b31cba0aa42c7904e4005adc6aea83f1ed417
gshearrer <grace.shearer@gmail.com>
2018-11-27 16:04:50 -0500

    added dry-pass edits

---
+++ BIDS/Heudiconv_drypass/dicominfo_grab.py
@@ -0,0 +1,41 @@
+# import packages
+
+import glob, os 
+import argparse
+import subprocess
+
+
+def set_parser():
+    global parser
+    global arglist
+    global args
+    parser=argparse.ArgumentParser(description='bids conversion')
+    parser.add_argument('-indir',dest='INDIR',
+                        default=False, help='enter directory where the sub-*/ directories are found')
+    parser.add_argument('-name',dest='STUDY',
+                        default=False, help='enter the study name, must be the same as found in the input path')
+    parser.add_argument('-out',dest='OUT',
+                        default=False, help='enter directory where you want the output to go')
+    parser.add_argument('-multisess',dest='SESS',
+                        action="store_true", help="if multiple session use this param")
+    parser.add_argument('-dcmpath',dest='DCMPATH',
+                        default=False, help='enter the unique dicom path (as described in docs)')
+
+
+    args = parser.parse_args()
+    arglist={}
+    for a in args._get_kwargs():
+        arglist[a[0]]=a[1]
+        
+
+        
+# get parameters         
+set_parser() 
+
+# grab dicominfo.tsv file by running heudiconv command --aka "DRY PASS"
+studyname = arglist["STUDY"]
+dcmpath=arglist["DCMPATH"]
+outpath = arglist["OUT"]
+newpath = dcmpath.replace(studyname, "{subject}")
+heudiconv_cmd = "singularity exec -B /:/test /projects/niblab/bids_projects/Singularity_Containers/heudiconv.simg heudiconv -d %s -s %s -f convertall -c none -o %s"%(newpath, studyname, outpath)
+run_batch=subprocess.Popen(["/Users/nikkibytes/Documents/TheBrainPipeline/BIDS/drypass.job", heudiconv_cmd])
