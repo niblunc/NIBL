8d4baded931c878e06ad558c76242162f5b9e99b
NikkiBytes <nicholletteacosta@gmail.com>
2019-01-21 20:14:38 -0500

    added BIDS GUI

--- TheBrainPipeline/BIDS/.ipynb_checkpoints/test-checkpoint.bat
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cmd=$1
-echo $cmd
--- TheBrainPipeline/BIDS/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import random
-heudiconv_cmd = "singularity exec -B /:/test /projects/niblab/bids_projects/Singularity_Containers/heudiconv.simg heudiconv -b "
-
-item = subprocess.Popen(["/Users/nikkibytes/Documents/TheBrainPipeline/BIDS/test.bat", heudiconv_cmd])
