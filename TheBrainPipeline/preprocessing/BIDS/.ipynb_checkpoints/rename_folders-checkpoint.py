811a0670da4f2ad6d0a7eb4b7801d577999e154f
NikkiBytes <nicholletteacosta@gmail.com>
2019-05-09 00:07:34 -0400

    added simpled edits

--- TheBrainPipeline/preprocessing/BIDS/.ipynb_checkpoints/rename_folders-checkpoint.py
+++ TheBrainPipeline/preprocessing/BIDS/.ipynb_checkpoints/rename_folders-checkpoint.py
@@ -6,16 +6,19 @@ import os, glob, shutil
 
 # Input Needed: directory path containing subject folders, output directory (if different than original)
 
-input_dir = '/projects/niblab/bids_projects/Experiments/bbx/DICOM'
+input_dir = '/projects/niblab/bids_projects/Experiments/bbx/DICOM/originals'
 
 FOLDERS = glob.glob(os.path.join(input_dir, "*"))
 
 for folder in FOLDERS:
     folder_name_split = folder.split("/")[-1].split("_")
-    print(folder_name)
-    study_name = folder_name.split("_")[0]
-    subject_id = folder_name.split("_")
+    print(folder_name_split)
+    study_name = folder_name_split[0]
+    subject_id = folder_name_split[1]
     print(study_name, subject_id)
-    #if folder_name.split("_") > 2: 
-     #   sess_id = folder_name.split("_")[2]
-      #  print(sess_id)
+    # Check the length here to see if we have multiple sessions
+    if len(folder_name_split) > 2: 
+        sess_id = folder_name_split[2]
+        print(sess_id)
+    new_name = "%s"%subject_id
+    
