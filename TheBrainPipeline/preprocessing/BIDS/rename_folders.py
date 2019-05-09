ce30ad2e4d8af4dcbeb04c2e394cba0a8b7d9b10
NikkiBytes <nicholletteacosta@gmail.com>
2019-05-09 13:20:31 -0400

    added edits to renaming file need to determine how to move forward with multi sess

--- TheBrainPipeline/preprocessing/BIDS/rename_folders.py
+++ TheBrainPipeline/preprocessing/BIDS/rename_folders.py
@@ -12,13 +12,26 @@ FOLDERS = glob.glob(os.path.join(input_dir, "*"))
 
 for folder in FOLDERS:
     folder_name_split = folder.split("/")[-1].split("_")
-    print(folder_name_split)
+    #print(folder_name_split)
     study_name = folder_name_split[0]
-    subject_id = folder_name_split[1]
-    print(study_name, subject_id)
+    subject_id = folder_name_split[1].split("-")[1]
+    #print(study_name, subject_id)
     # Check the length here to see if we have multiple sessions
     if len(folder_name_split) > 2: 
         sess_id = folder_name_split[2]
-        print(sess_id)
-    new_name = "%s"%subject_id
-    
+        multi_sess = True
+    else:
+        multi_sess = False
+        #print(sess_id)
+    new_name = ("%s"%subject_id).lower()
+    if "sub" not in new_name:
+        new_name = "sub-%s"%new_name
+    print(new_name)
+    # If multi sess is true, move the folders into the session directory
+    # first then rename in the folders 
+    if multi_sess == True:
+        print(">>> I am multiple sessions")
+        output_dir = os.path.join(input_dir, sess_id)
+        print(output_dir)
+        print("")
+        #shutil.move(folder, output_dir)
