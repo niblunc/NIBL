2db8c2fae4d7e47e7b2a4dee9df68ef41c6c5c46
NikkiBytes <nicholletteacosta@gmail.com>
2019-05-08 23:58:55 -0400

    added simpled edits

--- TheBrainPipeline/preprocessing/BIDS/rename_folders.py
+++ TheBrainPipeline/preprocessing/BIDS/rename_folders.py
@@ -6,4 +6,16 @@ import os, glob, shutil
 
 # Input Needed: directory path containing subject folders, output directory (if different than original)
 
-input_dir = input("")
+input_dir = '/projects/niblab/bids_projects/Experiments/bbx/DICOM'
+
+FOLDERS = glob.glob(os.path.join(input_dir, "*"))
+
+for folder in FOLDERS:
+    folder_name_split = folder.split("/")[-1].split("_")
+    print(folder_name)
+    study_name = folder_name.split("_")[0]
+    subject_id = folder_name.split("_")
+    print(study_name, subject_id)
+    #if folder_name.split("_") > 2: 
+     #   sess_id = folder_name.split("_")[2]
+      #  print(sess_id)
