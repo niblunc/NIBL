d0665178b07d22f5813fa59b156389b1d4bb318f
NikkiBytes <nicholletteacosta@gmail.com>
2019-05-08 23:25:55 -0400

    added renaming file

---
+++ TheBrainPipeline/preprocessing/BIDS/rename_folders.py
@@ -0,0 +1,9 @@
+import os, glob, shutil
+
+# This program is going to input original DICOM directories
+# that we will rename so our analysis will follow a consistent naming scheme. 
+
+
+# Input Needed: directory path containing subject folders, output directory (if different than original)
+
+input_dir = input("")
